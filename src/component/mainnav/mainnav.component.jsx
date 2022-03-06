590e06d96800f24879c71bf254f3005019d9f796
Prasad <dhebprasadshivaji@gmail.com>
2022-03-06 22:31:50 +0530

    solution overview done

--- src/component/mainnav/mainnav.component.jsx
+++ src/component/mainnav/mainnav.component.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import {BsBookHalf} from "react-icons/bs";
+
+// import {FaUserCircle} from 'react-icons/fa';
+import {FiLink2} from 'react-icons/fi';
+// import {IoAnalyticsOutline} from "react-icons/Io";
+import {MdGroup} from 'react-icons/md';
+// import {CgExport} from "react-icons/cg";
+import {BiTask} from 'react-icons/bi';
+import {ImStatsDots} from 'react-icons/im';
+import {FcUpload} from 'react-icons/fc';
+ import {RiAccountCircleLine} from 'react-icons/ri';
 import './mainnav.styles.css';
 export default function MainNav(){
     return(
 <div className="Mainnav">
-sds
+<div>
+<a href='/' className="ccenter">PxP Command Center</a>
+        <BsBookHalf className="book"/>
+
+        <MdGroup className="circle"/>
+
+        {/* <IoAnalyticsOutline/> */}
+
+        <ImStatsDots className="export"/>
+        <BiTask className="task"/>
+        <FcUpload className="upload"/>
+        <FiLink2 className="linkk"/>
+      <  RiAccountCircleLine className="profile"/>
+        </div>
 </div>
     );
 }
